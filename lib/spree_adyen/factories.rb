3f4aaebe8b3551dd42822726a02cee25c1c82a07
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-07-10 16:52:55 +0200

    CR fixes - factories

--- lib/spree_adyen/factories.rb
+++ lib/spree_adyen/factories.rb
@@ -1,13 +1,3 @@
-FactoryBot.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'spree_adyen/factories'
-
-  # Include the payment session factory
-  require 'spree_adyen/testing_support/payment_session_factory'
-  
-  # Include the payment source factories
-  require 'spree_adyen/testing_support/payment_source_factories'
-  require 'spree_adyen/testing_support/gateway_factory'
+Dir["#{File.dirname(__FILE__)}/testing_support/factories/**"].each do |f|
+  load File.expand_path(f)
 end
