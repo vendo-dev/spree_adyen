39cfd2585a1aa749c66449975676968073740603
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-07-10 14:54:06 +0200

    adyen cc happy path

--- app/javascript/spree_adyen/controllers/checkout_adyen_controller.js
+++ app/javascript/spree_adyen/controllers/checkout_adyen_controller.js
@@ -17,9 +17,34 @@ export default class extends Controller {
     const globalConfiguration = Object.assign(session, eventHandlers)
     const { AdyenCheckout, Dropin, Card, GooglePay, PayPal  } = window.AdyenWeb;
     const checkout = await AdyenCheckout(globalConfiguration);
-    const dropinConfiguration = {};
+    const dropinConfiguration = {
+      paymentMethodsConfiguration: {
+        card: {
+          onPaymentCompleted: (result, component) => {
+            // redirect to the payment session endpoint with the session result
+            const url = new URL(document.getElementById('dropin-container').dataset.checkoutAdyenPaymentSessionPathValue);
+            url.searchParams.set('sessionResult', result.sessionResult);
+            window.location.replace(url.href);
+          },
+          onPaymentFailed: (result, component) => {
+            console.info(result, component);
+          },
+          onError: (error, component) => {
+            console.error(error.name, error.message, error.stack, component);
+          },
+          showPayButton: false,
+          hasHolderName: true,
+        }
+      }
+    };
     const dropin = new Dropin(checkout, dropinConfiguration)
+    window.dropin = dropin;
 
     dropin.mount('#dropin-container');
+
+    document.getElementById('checkout-payment-submit').addEventListener('click', (e) => {
+      e.preventDefault();
+      dropin.submit();
+    });
   }
 } 
