33438fa7651288b1b1d5be76fdeccd9be21f55c3
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-07-17 10:43:43 +0200

    saving payment without source

--- app/services/spree_adyen/payment_sessions/process_with_result.rb
+++ app/services/spree_adyen/payment_sessions/process_with_result.rb
@@ -1,10 +1,6 @@
 module SpreeAdyen
   module PaymentSessions
     class ProcessWithResult
-      BRAND_MAP = {
-        'mc' => 'master'
-      }.freeze
-
       def initialize(payment_session:, session_result:)
         @payment_session = payment_session
         @session_result = session_result
@@ -35,42 +31,20 @@ module SpreeAdyen
       end
 
       def create_payment
-        Spree::Payment.create!(
-          order: order,
+        order.payments.build(
           amount: payment_session.amount,
-          source: nil,
           payment_method: payment_session.payment_method,
           response_code: payment_session.id,
           state: 'completed'
-        )
-      end
-
-      def payment_data
-        @payment_data ||= status_response.params.fetch('payments', [])[0]
-      end
-
-      def credit_card
-        return unless %w[googlepay scheme].include?(payment_data.fetch('paymentMethod')['type'])
-
-        Spree::CreditCard.find_or_create_by!(
-          payment_method_id: payment_session.payment_method_id,
-          gateway_payment_profile_id: additional_data.fetch('tokenization.storedPaymentMethodId')
-        ) do |new_credit_card|
-          new_credit_card.cc_type = mapped_brand_name
+        ).tap do |payment|
+          # saving payment without source cause in some cases
+          # response doesn't contain all required payment data
+          payment.save!(validate: false)
         end
-      end
-
-      def additional_data
-        @additional_data ||= status_response.params['additionalData'] || {}
       end
 
       def complete_order
         Spree::Dependencies.checkout_complete_service.constantize.call(order: order)
-      end
-
-      def mapped_brand_name
-        response_brand = additional_data['paymentMethod']
-        BRAND_MAP.fetch(response_brand, response_brand)
       end
 
       def status_response
