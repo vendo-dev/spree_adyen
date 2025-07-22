4398456adb2eed7e4203f78a5b456eba3bb3ca10
Damian Legawiec <damian@getvendo.com>
2025-07-22 17:03:29 +0200

    Update 20250630150000_setup_spree_adyen_models.rb

--- db/migrate/20250630150000_setup_spree_adyen_models.rb
+++ db/migrate/20250630150000_setup_spree_adyen_models.rb
@@ -9,7 +9,7 @@ class SetupSpreeAdyenModels < ActiveRecord::Migration[7.2]
       t.datetime :expires_at, null: false, index: true
       t.references :payment_method, null: false, index: true
       t.string :adyen_id, null: false, index: true
-      t.string :adyen_data, null: false
+      t.text :adyen_data, null: false
       t.timestamps
 
       t.index %w[payment_method_id adyen_id], unique: true
