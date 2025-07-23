33828737c55db97921ae775ed1dcd660b472edf7
Damian Legawiec <damian@getvendo.com>
2025-07-23 12:51:53 +0200

    Improve Payment Session model and introduce Create service
    * added currency validation (needs to be the same as orders currency)
    * auto-supply currency from order
    * adyen_id should be unique for entire system, not per payment method

--- app/services/spree_adyen/payment_sessions/find_or_create.rb
+++ app/services/spree_adyen/payment_sessions/find_or_create.rb
@@ -11,19 +11,7 @@ module SpreeAdyen
       def call
         return payment_session if payment_session.present?
 
-        response = payment_method.create_payment_session(amount, order)
-        return unless response.success?
-
-        PaymentSession.create!(
-          adyen_id: response.params['id'],
-          order: order,
-          amount: amount,
-          currency: order.currency,
-          adyen_data: response.params['sessionData'],
-          user: user,
-          expires_at: response.params['expiresAt'],
-          payment_method: payment_method
-        )
+        Create.new(order: order, user: user, amount: amount, payment_method: payment_method).call
       end
 
       private
