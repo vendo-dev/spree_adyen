c31be3d02b75105125d337c511bd66a69fbc7a56
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-08-12 14:36:36 +0200

    add reset column information

--- db/migrate/20250811140113_add_channel_to_adyen_payment_sessions.rb
+++ db/migrate/20250811140113_add_channel_to_adyen_payment_sessions.rb
@@ -2,6 +2,7 @@ class AddChannelToAdyenPaymentSessions < ActiveRecord::Migration[7.2]
   def change
     add_column :spree_adyen_payment_sessions, :channel, :string
 
+    SpreeAdyen::PaymentSession.reset_column_information
     SpreeAdyen::PaymentSession.where(channel: nil).update_all(channel: 'Web')
 
     add_index :spree_adyen_payment_sessions, :channel, algorithm: :concurrently
