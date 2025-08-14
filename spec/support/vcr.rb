4c2dbd008671e63881dd27856327fc9b300decca
Szymon Chorobski <szymon.chorobski@getvendo.com>
2025-08-14 13:07:03 +0200

    VCR fixes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -11,11 +11,18 @@ VCR.configure do |c|
   c.configure_rspec_metadata!
   c.default_cassette_options = { record: :new_episodes }
   c.filter_sensitive_data('<ADYEN_API_KEY>') { |i| i.request.headers['X-Api-Key']&.first }
+  c.filter_sensitive_data('<ADYEN_HMAC_KEY>') do |i|
+    body = i.response&.body.to_s
+    body[/\"hmacKey\":\"([A-F0-9]+)\"/, 1]
+  end
 
   c.before_record do |interaction|
     header_names = %w[X-Stripe-Client-User-Agent]
     headers = header_names.flat_map { |header_name| interaction.request.headers[header_name] }.compact
-
+    %w[Set-Cookie Requestid Pspreference Traceparent].each do |h|
+      interaction.response.headers.delete(h)
+    end
+    interaction.response.body.force_encoding('UTF-8')
     headers.each { |header| interaction.filter!(header, '<FILTERED>') }
   end
 end
